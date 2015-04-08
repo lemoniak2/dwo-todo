ad3509c76ed573b01ceb9f7b757dde04faa473e7
Swistaku <dev@blackflag.pl>
2015-04-08 17:20:29 +0200

    remove turbolinks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'items#index'
 
-  resources :items, except: :show do
+  resources :items, except: :show, path: '/' do
     resource :votes, only: %i(create destroy)
   end
 end
