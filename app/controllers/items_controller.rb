b4b163a12b637a5ce1abb7f992a627fd1d5e7a8e
Lukasz Gawel <l3moniak2@gmail.com>
2015-04-10 22:32:20 +0200

    Decent exposure

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,42 +1,29 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @items = Item.all
-  end
-
-  def new
-    @item = Item.new
-  end
+  expose(:items)
+  expose(:item, attributes: :item_params)
 
   def create
-    @item = Item.new(item_params)
-
-    if @item.save
-      redirect_to @item, notice: 'Item was successfully created.'
+    if item.save
+      redirect_to items_url, notice: 'Item was successfully created.'
     else
       render :new
     end
   end
 
   def update
-    if @item.update(item_params)
-      redirect_to @item, notice: 'Item was successfully updated.'
+    if item.save
+      redirect_to items_url, notice: 'Item was successfully updated.'
     else
       render :edit
     end
   end
 
   def destroy
-    @item.destroy
+    item.destroy
     redirect_to items_url, notice: 'Item was successfully destroyed.'
   end
 
   private
-
-  def set_item
-    @item = Item.find(params[:id])
-  end
 
   def item_params
     params.require(:item).permit(:title, :description, :image, :rating)
